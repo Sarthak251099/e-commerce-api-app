e4d0267084621201e2d1f445d08989ff27e679d7
Sarthak Surana <sarthak251099@gmail.com>
2024-07-29 14:07:07 +0530

    Added api documenatation

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -17,20 +17,6 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-    # def test_create_user_with_existing_email(self):
-    #     """Test creating a user with an email already exists."""
-    #     email = 'test@example.com'
-    #     password = 'testpass123'
-    #     user = get_user_model().objects.create_user(
-    #         email=email,
-    #         password=password,
-    #     )
-
-    #     new_user = get_user_model().objects.create_user(
-    #         email=email,
-    #         password="Testpass_newpass12",
-    #     )
-
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
         sample_emails = [
